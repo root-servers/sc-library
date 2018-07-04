0eedb14df7bcb16edc8a12ed6a8fc1e812956fd5
Nikolay Toporkov <toporkov.kolya96@gmail.com>
2018-07-04 18:06:48 +0300

    add erc223 mintable token

---
+++ contracts/ERC223/ERC223MintableToken.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.4.23;
+
+import "openzeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
+import "./ERC223Token.sol";
+
+
+contract ERC223MintableToken is MintableToken, ERC223Token {
+    function mint(
+        address _to,
+        uint256 _amount
+    )
+        hasMintPermission
+        canMint
+        public
+        returns (bool)
+    {
+        bytes memory empty;
+        totalSupply_ = totalSupply_.add(_amount);
+        balances[_to] = balances[_to].add(_amount);
+        if (_to.isContract()) {
+            ERC223Receiver receiver = ERC223Receiver(_to);
+            receiver.tokenFallback(address(this), _amount, empty);
+        }
+        emit Mint(_to, _amount);
+        emit Transfer(msg.sender, _to, _amount, empty);
+        return true;
+    }
+}
