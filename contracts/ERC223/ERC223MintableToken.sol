67e7b1783d03021b89ed90e318ac5b866aaa94b2
Nikolay Toporkov <toporkov.kolya96@gmail.com>
2018-08-03 18:59:00 +0300

    code covered by docs

--- contracts/ERC223/ERC223MintableToken.sol
+++ contracts/ERC223/ERC223MintableToken.sol
@@ -4,7 +4,17 @@ import "openzeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
 import "./ERC223Token.sol";
 
 
+/**
+ * @title ERC223MintableToken
+ * @dev ERC223 implementation of MintableToken.
+ */
 contract ERC223MintableToken is MintableToken, ERC223Token {
+  /**
+   * @dev Function to mint tokens. Invokes token fallback function on recipient address.
+   * @param _to The address that will receive the minted tokens.
+   * @param _amount The amount of tokens to mint.
+   * @return A boolean that indicates if the operation was successful.
+   */
   function mint(
     address _to,
     uint256 _amount
